516b54ae120cfb63a23adf669921521b8d07268c
NotOriginal333 <m.naumovych2@gmail.com>
2025-07-19 22:11:24 +0300

    feat: added celery queues for media, follow, default and 2 more workers with containers

--- api/socialnet/config/celery.py
+++ api/socialnet/config/celery.py
@@ -7,4 +7,22 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.task_queues = {
+    'media': {
+        'exchange': 'media',
+        'routing_key': 'media',
+    },
+    'follows': {
+        'exchange': 'follows',
+        'routing_key': 'follows',
+    }
+}
+
+app.conf.task_default_queue = 'default'
+
+app.conf.task_routes = {
+    'images.*': {'queue': 'media'},
+    'apps.follows.tasks.*': {'queue': 'follows'},
+}
+
 app.autodiscover_tasks()
